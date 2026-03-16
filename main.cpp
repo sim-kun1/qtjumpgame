f2ea67ba171befa797560a07636bb91af38e9b84
福坤 徐 <sim20061123@163.com>
2026-03-16 22:33:29 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿#include<QApplication>
+#include<QWidget>
+#include<QPainter>
+#include<QPaintEvent>
+#include<QPushButton>
+#include<QVBoxLayout>
+#include"mainWindow.h"
+
+void mainWindow::paintEvent(QPaintEvent*) {
+
+	//绘制主页面
+	QPainter p(this);
+	p.fillRect(rect(), QColor(249, 244, 220));
+	p.setBrush(Qt::red);
+	p.drawRect(260, 200, 80, 60);
+	p.setBrush(Qt::yellow);
+	p.drawRect(440, 195, 120, 70);
+	p.setBrush(Qt::blue);
+	p.drawRect(470, 60, 60, 80);
+	p.setBrush(Qt::white);
+	p.drawEllipse(280, 210, 40, 40);
+	p.setPen(QColor(10, 10, 20));
+	p.setFont(QFont("Arial", 42, QFont::Bold));
+	p.drawText(rect().translated(2, 2), Qt::AlignHCenter | Qt::AlignTop, "JumpGame");
+	p.setPen(QColor(102, 193, 140));
+	p.drawText(rect(), Qt::AlignHCenter | Qt::AlignTop, "JumpGame");
+
+
+}
+
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+
+	mainWindow gameWin;
+
+	gameWin.show();
+	return app.exec();
+}
---
+++ mainWindow.h
@@ -0,0 +1,43 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include<QWidget>
+#include<QPainter>
+#include<QPaintEvent>
+#include<QPushButton>
+#include<QVBoxLayout>
+
+class mainWindow :public QWidget {
+	Q_OBJECT
+public:
+	mainWindow(QWidget* parent = nullptr) :QWidget(parent) {
+		//创建窗口
+		setFixedSize(600, 400);
+		this->setWindowTitle("JumpGame");
+		this->resize(600, 400);
+		this->setFocusPolicy(Qt::StrongFocus);
+		//创建控件
+		QPushButton* btnStart = new QPushButton("Start", this);
+		btnStart->setFixedSize(180, 80);
+		btnStart->move(210, 300);
+		btnStart->setStyleSheet(R"(
+		QPushButton {
+		    background-color: #4285F4;
+		    color: white;
+		    border-radius: 18px;
+		    font-size: 18px;
+		    font-weight: bold;
+		}
+		QPushButton:hover {
+		    background-color: #5a95f5;
+		}
+		QPushButton:pressed {
+			background-color: #3367d6;
+		}
+		)");
+	}
+protected:
+	void paintEvent(QPaintEvent*) override;
+};
+
+#endif
